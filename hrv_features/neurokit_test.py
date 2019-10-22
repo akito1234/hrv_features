bb2ebfd48aa7bf97bf05b85ac7fffe964bfeb9fc
章人 東間 <4516082@ed.tus.ac.jp>
2019-10-23 01:29:24 +0900

    EDAの解析の追加

--- hrv_features/neurokit_test.py
+++ hrv_features/neurokit_test.py
@@ -4,32 +4,39 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 from opensignalsreader import OpenSignalsReader
+from biosppy.signals import eda
+
 
 # Download data
-path = r"\\Ts3400defc\共有フォルダ\theme\mental_stress\02.BiometricData\2019-10-22\kishida\opensignals_dev_2019-10-22_13-54-50.txt"
+path = r"C:\Users\akito\Desktop\stress\02.BiometricData\2019-10-21\opensignals_201806130003_2019-10-21_15-16-48.txt"
 
 # Read OpenSignals file and plot all signals
 arc = OpenSignalsReader(path)
-eda = arc.signal('EDA')
-
-# Process the resp signals
-onsets, peaks, amplitudes, recoveries = nk.eda_scr(eda)
-
-
-plt.figure(figsize=(24,8))
-plt.plot(arc.t,eda)
-for onset, peak, amplitude, recovery in zip(onsets, peaks, amplitudes, recoveries):
-    plt.axvline(onset*0.001,color="r")
-    plt.axvline(peak*0.001,alpha=0.3,color="b")
-    plt.axvline(recovery*0.001,alpha=0.3,color="y")
+eda2 = arc.signal('EDA')
+eda.eda(eda2, sampling_rate=1000.0, show=True, min_amplitude=0.1)
 plt.show()
 
+## Process the resp signals
+#processed_eda = nk.eda_process(eda)['EDA']
 
-resp_data  = np.c_[np.array(onsets),
-                   np.array( peaks),
-                   np.array(amplitudes),
-                   np.array(recoveries)
-                   ]
-np.savetxt(r"C:\Users\akito\Desktop\eda_kishida.csv"
-            ,resp_data,delimiter=',')
+#plt.figure(figsize=(24,8))
+#plt.plot(arc.t,eda)
+#for onset, peak, recovery in zip(processed_eda['SCR_Onsets'],
+#                                 processed_eda['SCR_Peaks_Indexes'],
+#                                 processed_eda['SCR_Recovery_Indexes']):
+#    plt.axvline(onset*0.001,color="r")
+#    plt.axvline(peak*0.001,alpha=0.3,color="b")
+##   plt.axvline(recovery*0.001,alpha=0.3,color="y")
+#plt.show()
+
+
+#eda_data  = np.c_[processed_eda['SCR_Onsets'],
+#                   processed_eda['SCR_Peaks_Indexes'],
+#                   processed_eda['SCR_Peaks_Amplitudes'],
+#                   processed_eda['SCR_Recovery_Indexes']
+#                   ]
+
+#np.savetxt(r"C:\Users\akito\Desktop\eda_tohma.csv"
+#            ,eda_data,delimiter=',')
