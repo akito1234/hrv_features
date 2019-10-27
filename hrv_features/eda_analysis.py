66e4ca8934e8bc4d1e00f7b702ac3ace90467f8a
章人 東間 <4516082@ed.tus.ac.jp>
2019-10-28 00:07:16 +0900

    心電図から心拍の推定

--- hrv_features/eda_analysis.py
+++ hrv_features/eda_analysis.py
@@ -1,7 +1,8 @@
 # Import Package
-from biosppy.signals.eda import eda
+from biosppy.signals import eda
 import numpy as np
 import pandas as pd
+import matplotlib.pylab as plt
 from opensignalsreader import OpenSignalsReader
 
 
@@ -30,8 +31,26 @@ def EDA_FEATURES(eda,section):
     return eda_features
 
 if __name__ == '__main__':
-    path = r"C:\Users\akito\Desktop\stress\02.BiometricData\2019-10-14\opensignals_dev_2019-10-14_11-40-05.txt"
+    path = r"C:\Users\akito\Desktop\stress\02.BiometricData\2019-10-23\teraki\opensignals_dev_2019-10-23_16-59-10.txt"
     arc = OpenSignalsReader(path)
-    eda_data = eda(arc.signal('EDA'),show=True)
-    for keys in EDA_FEATURES(eda_data):
-        print(keys, EDA_FEATURES(eda_data)[keys])
+    #np.savetxt(r"C:\Users\akito\Downloads\sample_E4\EDA.csv",arc.signal('EDA'),
+    #           delimiter = ",")
+    eda_filtered = eda.eda(arc.signal('EDA'),show=False)
+    eda_data = eda.basic_scr(eda_filtered['filtered'], sampling_rate=1000.0)
+    fig,axs = plt.subplots(2,1,sharex=True)
+    axs[0].plot(arc.t,eda_filtered['filtered'])
+    for onset, peak in zip(eda_data['onsets'],eda_data['peaks']):
+        axs[0].axvline(onset*0.001,color="r")
+        axs[0].axvline(peak*0.001,color="b")
+    onsets_amplitide = eda_filtered['filtered'][eda_data['onsets'].tolist()]
+    axs[1].plot(eda_data['peaks']*0.001,eda_data['amplitudes']-onsets_amplitide)
+    plt.show()
+
+eda_output  = np.c_[eda_data['onsets'],eda_data['peaks'],(eda_data['amplitudes']-onsets_amplitide)]
+
+np.savetxt(r"C:\Users\akito\Desktop\eda_tohma.csv"
+            ,eda_output,delimiter=',')
+
+
+    #for keys in EDA_FEATURES(eda_data):
+    #    print(keys, EDA_FEATURES(eda_data)[keys])
