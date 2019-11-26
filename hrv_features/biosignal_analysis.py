31b1c0db5c38e86a097a77e71ed2be981e435c87
章人 東間 <akito.tohma@gmail.com>
2019-11-26 16:52:32 +0900

    Modify a little

--- hrv_features/biosignal_analysis.py
+++ hrv_features/biosignal_analysis.py
@@ -18,7 +18,7 @@ def correction_neutral_before(df_neutral,df_emotion,identical_parameter):
     #不要なパラメータのを除き，Neuralで補正
     df_neutral_features = df_neutral.drop(identical_parameter, axis=1)
     df_emotion_features = df_emotion.drop(identical_parameter, axis=1)
-    features_df = (df_emotion_features - df_neutral_features.values)
+    features_df = (df_emotion_features-df_neutral_features.values)
 
     result = pd.concat([identical_df,features_df], axis=1,sort=False)
     return result
@@ -59,13 +59,23 @@ def features_baseline(df,emotion_state=['Stress','Ammusement','Neutral2'],
 #---------------------------------------------
 # Plot features list by using barplot
 #---------------------------------------------
-def features_barplot(df,columns=None,sort_order = ['Stress','Ammusement','Neutral2']):
+def features_barplot(df,columns=None, emotion_status = ['Neutral2','Stress'],annotation=True):
+    # データフレームにフィルタをかける
+    df = df[df['emotion'].isin(emotion_status)]
+
     fig, axes = plt.subplots(1,len(columns))
     for i,column in enumerate(columns):
-        sns.barplot(x='emotion', y=column, data=df, ax=axes[i],order=sort_order, capsize=.1)
-        
-        
+        sns.barplot(x='emotion', y=column, data=df, ax=axes[i], order=emotion_status, capsize=.1)
 
+        if annotation:
+            if i == 0:
+                # コルモゴロフ-スミルノフ検定 2標本の検定
+                p_value = K_S_test(df,emotion_status = emotion_status, identical_parameter = ['id','emotion','user','date','path_name'])
+            # 統計指標を表示
+            max_value = df[column].mean() + df[column].std()
+            y, h, col = max_value, max_value * 0.05, 'k'
+            axes[i].plot([0, 0, 1, 1], [y, y+h, y+h, y], lw=1.5, c=col)
+            axes[i].text(.5, y+h, "p={:.3f}".format(p_value[column]), ha='center', va='bottom', color=col)
 
 #---------------------------------------------
 # コルモゴロフ-スミルノフ検定
@@ -75,7 +85,8 @@ def K_S_test(df,emotion_status = ['Neutral2','Stress'], identical_parameter = ['
         print('emotion_statusには感情名を2つ入れる')
         return False
     result = {}
-
+    print("Kolmogorov–Smirnov test")
+    print("{} vs {}".format(emotion_status[0],emotion_status[1]))
     for column in df.columns:
         # 関連コラムは飛ばす
         if identical_parameter.count(column) > 0:
@@ -90,15 +101,14 @@ def K_S_test(df,emotion_status = ['Neutral2','Stress'], identical_parameter = ['
 
         print('{} : {} '.format(column,p_value))
 
-    # convert to Dataframe
-    result = pd.DataFrame.from_dict(result, orient='index')
+    ## convert to Dataframe
+    #result = pd.DataFrame.from_dict(result, orient='index')
     return result
 
 
 
 if __name__ == '__main__':
     import matplotlib as mpl
-    #plt.style.use('ggplot') 
     font = {'family' : 'meiryo'}
     plt.rcParams["font.size"] = 18
 
@@ -106,7 +116,7 @@ if __name__ == '__main__':
     path = r"Z:\theme\mental_stress\03.Analysis\Analysis_Features\実験結果 2019_11_19~21\biosignal_datasets_arousal_valence.xlsx"
     df = pd.read_excel(path)
 
-
+    # 正規化 (個人差補正)
     df_features = features_baseline(df,emotion_state=['Neutral2','Stress','Ammusement'],baseline='Neutral1')
 
     # 描画設定
@@ -116,23 +126,22 @@ if __name__ == '__main__':
                'pathicData_mean'
                ]
 
-    features_barplot(df_features[ df_features['emotion'] != 'Neutral2'],
-                     columns, 
-                     sort_order = ['Stress','Ammusement'])
+    features_barplot(df_features[~df['id'].isin([5,11,13])],columns,emotion_status = ['Ammusement','Stress'])
+
+
     #sns.pairplot(data=df_features, 
     #             hue='emotion',
     #             vars=columns
     #            )
     
     # コルモゴロフ-スミルノフ検定
-    A = K_S_test(df_features,emotion_status = ['Stress','Ammusement'])
-    #A.to_excel(r"Z:\theme\mental_stress\03.Analysis\Analysis_Features\実験結果 2019_11_19~21\K_S_p_value.xlsx")
+    #A = K_S_test(df_features,emotion_status = ['Stress','Ammusement'])
 
-    
-
-    #colorlist = ["b","r", "g"]
-    #ax = sns.barplot(x='id', y=df['pathicData_mean'], hue='emotion', data=df[ df['emotion'] != 'Ammusement'],palette	= colorlist)
-    #ax.legend().set_visible(False)
+    colorlist = ["b","r","g","y"]
+    #ax = sns.barplot(x='id', y=df['fft_ratio'], hue='emotion', 
+    #                 data=df#[ (df['emotion'] != 'Stress') & (df['emotion'] != 'Ammusement')]
+    #                 ,palette= colorlist)
+    #ax.legend()#.set_visible(False)
     #features_barplot(df_features[ df_features['emotion'] != 'Ammusement'],
     #                 columns, 
     #                 sort_order = ['Stress','Neutral2'])
