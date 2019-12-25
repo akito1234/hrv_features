d7b0d30a9c384e18036a4ce79261dba9f016808d
章人 東間 <4516082@ed.tus.ac.jp>
2019-12-25 18:34:19 +0900

    主観評価を用いたデータ選定を実装

--- hrv_features/Support_Vector_Classifier_.py
+++ hrv_features/Support_Vector_Classifier_.py
@@ -34,6 +34,8 @@ def get_datasets(question_path,dataset_path, remove_label=["Neutral2"],
     # 特徴量を取得する
     features = pd.read_excel(dataset_path)
     features = features[~features["emotion"].isin(remove_label)]
+    # 不要な特徴量を削除
+    features =  features.drop(["nni_diff_min"],axis=1)
     if normalization:
         # 正規化 (個人差補正)
         features = features_baseline(features,emotion_state=['Stress','Amusement'],baseline='Neutral1',
@@ -48,8 +50,39 @@ def get_datasets(question_path,dataset_path, remove_label=["Neutral2"],
 ## 前処理
 ##-----------------------------------
 # アンケート結果によるフィルタ
-def questionnaire_filter():
+def extension_affectgrid(questionnaire):
+    # AFFECT GRIDの感情の強さと，方向を決める
+    _arousal = questionnaire["Arousal"].values
+    _valence = questionnaire["Valence"].values
+    questionnaire["strength"] = np.sqrt( np.square(_arousal) + np.square(_valence) )
+    questionnaire["angle"]  = np.arctan2(_arousal/ _valence)
+    return questionnaire
+
+# アンケート結果に基づいたデータの選定
+def emotion_label_filter(qna):
+
+    df_amusement = qna.query('(Arousal > 4 & Valence > 4) | Emotion_1 in (1, 2, 3, 9, 10, 11, 15)')
+    df_stress = qna.query('(Arousal > 4 & Valence < 4) | Emotion_1 in (4, 5, 6, 7, 8, 12, 13, 14)')
+
+
+    print(df_stress.head())
+
+    #df_amusement = questionnaire[ (questionnaire["Arousal"] > 4) & (questionnaire["Valence"] > 4)]
+    #df_amusement["emotion"] = "Amusement"
+
+    #df_stress = questionnaire[ (questionnaire["Arousal"] > 4) & (questionnaire["Valence"] < 4)]
+    #df_stress["emotion"] = "Stress"
+    #result = pd.concat([df_amusement,df_stress],axis = 1)
+
+    ## 出力
+    ## 誰から何個のデータが失われたかを出力する
+
+    #return result
     pass
+def emotion_info(questionnaire):
+    
+    pass
+
 # Affect Grid の拡張
 def Extension_AffectGrid():
     # 感情の強さ
@@ -71,7 +104,7 @@ def get_targets(df, target_label = "emotion",type="label"):
 # データセットから特徴量を抽出
 # 最終的には，ここで特徴量選択を行う
 def get_features(df,drop_features = ['id','emotion','user','date','path_name','Valence','Arousal',
-                                     'Emotion_1','Emotion_2',"nni_diff_min"]):
+                                     'Emotion_1','Emotion_2']):
     df_features = df.drop(drop_features, axis=1)
     # スケール調整
     # 平均値を0,標準偏差を1 
@@ -84,7 +117,7 @@ def get_features(df,drop_features = ['id','emotion','user','date','path_name','V
 ## モデル構築
 ##--------------------------
 # 線形サポートベクタを用いて最適なパラメータを推定する
-def Model_tuning(df):
+def model_tuning(df):
     targets = get_targets(df, target_label = "emotion",type="label")
     features = get_features(df)
     #penaltyとlossの組み合わせは三通り
@@ -95,11 +128,11 @@ def Model_tuning(df):
 
     # LinearSVCの取りうるモデルパラメータを設定
     C_range= np.logspace(-2, 1, 6)
-    param_grid = [{"penalty": ["l2"],"loss": ["hinge"],"dual": [True],"max_iter":[5000],
+    param_grid = [{"penalty": ["l2"],"loss": ["hinge"],"dual": [True],"max_iter":[8000],
                    'C': C_range, "tol":[1e-3]}, 
-                  {"penalty": ["l1"],"loss": ["squared_hinge"],"dual": [False],"max_iter":[5000],
+                  {"penalty": ["l1"],"loss": ["squared_hinge"],"dual": [False],"max_iter":[8000],
                    'C': C_range, "tol":[1e-3]}, 
-                  {"penalty": ["l2"],"loss": ["squared_hinge"],"dual": [True],"max_iter":[5000],
+                  {"penalty": ["l2"],"loss": ["squared_hinge"],"dual": [True],"max_iter":[8000],
                    'C': C_range, "tol":[1e-3]}]
 
     gkf = split_by_group(df)
@@ -134,40 +167,58 @@ def split_by_group(dataset):
     return gkf
 
 # 特徴量選択
-def feature_selection(dataset):
+def feature_selection(dataset,show=False):
+    drop_label = ['id','emotion','user','date','path_name','Valence','Arousal',
+                                    'Emotion_1','Emotion_2']
     targets = get_targets(dataset, target_label = "emotion",type="label")
-    features = get_features(dataset)
+    features = get_features(dataset,drop_label)
     gkf = split_by_group(dataset)
-
-    ## 反復特徴量選択
-    #select = RFECV(RandomForestClassifier(n_estimators=100,random_state=42)
-    #               ,cv=gkf, scoring='average_precision')
-    #select.fit(features, targets)
-    
 
     # define random forest classifier, with utilising all cores and
     # sampling in proportion to y labels
     rf = RandomForestClassifier(n_jobs=int(cpu_count()/2), max_depth=7)
 
     # define Boruta feature selection method
-    feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=42)
+    feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, two_step=False, random_state=42,max_iter=100)
 
     # find all relevant features - 5 features should be selected
     feat_selector.fit(features, targets)
+
     # 出力
-    print('Number of select feature: {}'.format(feat_selector.support_))
-    print('Features : \n{}'.format(feat_selector.ranking_))
+    column_list = dataset.columns.drop(drop_label).tolist()
+    print('\n Initial features: {}'.format(column_list))
+    # number of selected features
+    print('\n Number of select feature: {}'.format(feat_selector.n_features_))
 
-    ## 可視化
-    mask = select.support_
-    plt.matshow(mask.reshape(1, -1), cmap='gray_r')
-    plt.tick_params(labelleft = 'off')
-    plt.xlabel('使われた特徴. 黒が選択されたもの', fontsize=15)
-    plt.show()
+    print ('\n Top %d features:' % feat_selector.n_features_)
+    feature_df = pd.DataFrame(column_list, columns=['features'])
+    feature_df['rank']=feat_selector.ranking_
+    feature_df = feature_df.sort_values('rank', ascending=True).reset_index(drop=True)
+    print (feature_df.head(feat_selector.n_features_))
 
-    return select.n_features_
-    pass
+    # check ranking of features
+    print ('\n Feature ranking:')
+    print (feat_selector.ranking_)
 
+    ## call transform() on X to filter it down to selected features
+    #X_filtered = feat_selector.transform(X)
+    
+    # 特徴量後のデータセット作成
+    selected = dataset.drop(drop_label, axis=1).columns[feat_selector.support_]
+    #selected = dataset.drop(drop_label, axis=1).columns[feat_selector.support_ | feat_selector.support_weak_]
+    selected_dataset = dataset[selected]
+    result = pd.concat([dataset[drop_label],selected_dataset], axis=1)
+
+    if show:
+        ## 可視化
+        mask = feat_selector.support_
+        # マスクを可視化する．黒が選択された特徴量
+        plt.matshow(mask.reshape(1, -1), cmap='gray_r')
+        plt.tick_params(labelleft = 'off')
+        plt.xlabel('Sample Index', fontsize=15)
+        plt.show()
+
+    return result
 
 
 ## ---------------------------------
@@ -269,27 +320,43 @@ def feature_selection(dataset):
 
 if __name__ =="__main__":
     question_path = r"C:\Users\akito\Desktop\stress\03.Analysis\Analysis_Features\QuestionNaire_result.xlsx"
-    dataset_path = r"C:\Users\akito\Desktop\stress\03.Analysis\Analysis_Features\biosignal_datasets_1.xlsx"
-    dataset = get_datasets(question_path,dataset_path)
-    dataset.to_excel(r"C:\Users\akito\Desktop\test_2.xlsx")
-    # info
-    print("----- Strat Machine learning -----")
-    print("Info: \n data num: {} \n feature num: {}".format(dataset.shape[0],dataset.shape[1]))
-    print("\n feature label : \n {}".format(dataset.columns))
-    print("\n emotion label : {}".format(dataset["emotion"].unique()))
-    
-    # 特徴量選択
-    selected_features_ = feature_selection(dataset)
-    
-    # モデル構築
-    print("----- Model Tuning -----")
-    coef_ = Model_tuning(dataset)
-    
+    # アンケート結果を取得する
+    questionnaire = pd.read_excel(question_path,sheet_name="questionnaire",usecols=[i for i in range(12)])
+    #df = emotion_label(questionnaire)
+    #df.to_excel(r"C:\Users\akito\Desktop\questionNaire.xlsx")
+    emotion_label_filter(questionnaire)
 
-    # 精度検証
+    #dataset_path = r"C:\Users\akito\Desktop\stress\03.Analysis\Analysis_Features\biosignal_datasets_1.xlsx"
+    #dataset = get_datasets(question_path,dataset_path,normalization=True)   
 
-    # 可視化
+    ## Valenceは残す
+    #df = dataset.drop(['id','emotion','user','date','path_name','Arousal',
+    #                          'Emotion_1','Emotion_2'],axis=1)
+    #res=df.corr().to_excel(r"C:\Users\akito\Desktop\cor_python.xlsx")   # pandasのDataFrameに格納される
+    #print(res)
+    ## info
+    #print("\n----- Strat Machine learning -----")
+    #print("Info: \n data num: {} \n feature num: {}".format(dataset.shape[0],dataset.shape[1]))
+    #print("\n feature label : \n {}".format(dataset.columns))
+    #print("\n emotion label : {}".format(dataset["emotion"].unique()))
+    
+    #print("\n----- Model Tuning -----")
+    ## モデル構築
+
+    ## 特徴量選択
+    #selected_dataset = feature_selection(dataset)
+    #selected_dataset.to_excel(r"C:\Users\akito\Desktop\feature_selected.xlsx")
+
+    ## GridSearch
+    #coef_2 = model_tuning(dataset)
+    #print("\n特徴量選択後")
+    ## 相関ある特徴量と，ありそうな特徴量を使用
+    #coef_ = model_tuning(selected_dataset)
+
+    ## 精度検証
+
+    ## 可視化
 
     
-    pass
+    #pass
 
