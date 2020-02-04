caeb1407521b0125d4b1ea6ad22835b72ff84812
章人 東間 <akito.tohma@gmail.com>
2020-02-04 22:03:13 +0900

    ;

--- hrv_features/src/LinearSVC/trainer.py
+++ hrv_features/src/LinearSVC/trainer.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib
 
 import matplotlib.pyplot as plt
-#del matplotlib.font_manager.weight_dict['roman']
+del matplotlib.font_manager.weight_dict['roman']
 matplotlib.font_manager._rebuild()
 
 
@@ -16,12 +16,12 @@ from sklearn.svm import SVC
 # ---------------------------------------
 #　設定
 # ---------------------------------------
-predict_path = r"C:\Users\akito\Desktop\Hashimoto\robot_communication\04_Analysis\Analysis_TimeVaries\features_kishida_2020-01-28.xlsx"
+predict_path = r"Z:\theme\robot_communication\04_Analysis\Analysis_TimeVaries\features_tohma_2020-01-28.xlsx"
 selected_features =  ['ar_peak_lf', 'lomb_rel_hf', 'nni_min', 'nni_max', 'hr_max', 'nn50', 'tinn', 'sampen', 'bvp_min', 'sc_mean']
 model = "LinearSVM_ALL_FowardFeaturesSelection_2020_02_01.pickle"
 emotion_label = ["Amusement","Neutral", "Stress"]
 emotion_color = ["b","gray", "r"]
-#time_record_path = r"Z:\theme\robot_communication\03_LogData\2020-01-28\kishida\robot_communication_2020_01_28__19_29_33.xlsx"
+time_record_path = r"Z:\theme\robot_communication\03_LogData\2020-01-28\tohma\robot_communication_2020_01_28__20_51_22.xlsx"
 
 
 # 検証用のデータ
@@ -68,62 +68,53 @@ digit_score = clf.predict_proba(selected_test)
 print(digit_score)
 #np.savetxt(r"C:\Users\akito\Desktop\test_output.csv",digit_score,delimiter=",")
 
-## 描画
-#time_record = pd.read_excel(time_record_path,header=0,index_col = 0)
-#end_time = (time_record.loc["Amusement","FinishDatetime"] - time_record.loc["Neutral","StartDatetime"]).total_seconds()
-#stress_start = (time_record.loc["Stress archimetic","FinishDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
-#stress_finish = (time_record.loc["Stress archimetic","StartDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
-#amusement_start = (time_record.loc["Amusement","StartDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
-#amusement_finish = (time_record.loc["Amusement","FinishDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
+# 描画
+time_record = pd.read_excel(time_record_path,header=0,index_col = 0)
+end_time = (time_record.loc["Amusement","FinishDatetime"] - time_record.loc["Neutral","StartDatetime"]).total_seconds()
+stress_start = (time_record.loc["Stress archimetic","FinishDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
+stress_finish = (time_record.loc["Stress archimetic","StartDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
+amusement_start = (time_record.loc["Amusement","StartDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
+amusement_finish = (time_record.loc["Amusement","FinishDatetime"]  - time_record.loc["Neutral","StartDatetime"]).total_seconds()
 
-#print("End Time[s] : {}".format(end_time))
-plt.figure(figsize= (29,13))
+print("End Time[s] : {}".format(end_time))
+plt.figure(figsize= (29,18))
 
 plt.rcParams["font.family"]="Times New Roman"
-plt.rcParams["font.size"]=50
+plt.rcParams["font.size"]=120
 plt.rcParams["xtick.direction"]="in"
 plt.rcParams["ytick.direction"]="in"
-plt.rcParams['xtick.major.width'] = 3.0#x軸主目盛り線の線幅
-plt.rcParams['ytick.major.width'] = 3.0#y軸主目盛り線の線幅
-plt.rcParams['axes.linewidth'] = 3.0# 軸の線幅edge linewidth。囲みの太さ
+plt.rcParams['xtick.major.width'] = 5.0#x軸主目盛り線の線幅
+plt.rcParams['ytick.major.width'] = 5.0#y軸主目盛り線の線幅
+plt.rcParams['axes.linewidth'] = 5.0# 軸の線幅edge linewidth。囲みの太さ
 plt.rcParams["legend.markerscale"] = 2
 plt.rcParams["legend.fancybox"] = False
-plt.rcParams["legend.framealpha"] = 1
+plt.rcParams["legend.framealpha"] = 10
 plt.rcParams["legend.edgecolor"] = 'black'
-
-#plt.step(time[1:],accuracy==0,label = emotion_label[0])
-#plt.step(time[1:],accuracy==2,label = emotion_label[2])
+plt.rcParams["legend.labelspacing"] =0.01
+plt.rcParams["legend.handletextpad"] = 0.01
 plt.tick_params(length = 20)
-
+plt.rcParams['patch.linewidth'] = 5
 
 for i in range(len(emotion_label)):
-    plt.plot(time[1:], digit_score[:,i]*100, emotion_color[i]
-             ,label = emotion_label[i],linewidth = 4.5)
+    plt.plot(time[1:], digit_score[:,i]*100, emotion_color[i],label = emotion_label[i],linewidth = 10)
 
-
-    #plt.xlim(300,end_time)
-#plt.axvspan(amusement_start,amusement_finish,alpha=0.1,color="b"
-#            #,label="Amusement Section"
-#            )
-#plt.axvspan(stress_start, stress_finish,alpha=0.1,color="r"
-#            #,label="Stress Section"
-#            )
-
-plt.axvspan(200,720,alpha=0.1,color="r"
+plt.axvspan(amusement_start,1200,alpha=0.1,color="r"
             #,label="Amusement Section"
             )
-plt.axvspan(850, 1200,alpha=0.1,color="b"
+plt.axvspan(stress_start, stress_finish,alpha=0.1,color="b"
             #,label="Stress Section"
             )
 
-#plt.legend(bbox_to_anchor=(0.5,-0.06), loc='upper center',ncol=3, fontsize=35)
-#plt.xlim(330,end_time)
-plt.xlim(330,1200)
 
-#plt.tight_layout()
+
+
+
 plt.xlabel("Time [s]")
-plt.ylim(0,100)
-plt.yticks(np.arange(0,120,20))
 plt.ylabel("Probability [%]")
+plt.yticks(np.arange(0,120,20))
+plt.xticks(np.arange(0,1300,200))
+plt.ylim(0,100)
+plt.xlim(350,1200)
+plt.legend(bbox_to_anchor=(0.5,-0.25), loc='upper center',ncol=3, fontsize=100)
 #plt.show()
 plt.savefig(r"C:\Users\akito\Desktop\Figure_1.png", bbox_inches="tight", pad_inches=0.05)
